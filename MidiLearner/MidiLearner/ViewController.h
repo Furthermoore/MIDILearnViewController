6c2aa179eb4a6f4842461c38fa19b10c0b66615c
Dan Moore <dhmoore@berklee.edu>
2015-07-04 04:04:46 -0400

    installing project in repo

---
+++ MidiLearner/MidiLearner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MidiLearner
+//
+//  Created by Dan Moore on 7/4/15.
+//  Copyright (c) 2015 FurtherMoore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MidiLearner/MidiLearner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MidiLearner
+//
+//  Created by Dan Moore on 7/4/15.
+//  Copyright (c) 2015 FurtherMoore. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
